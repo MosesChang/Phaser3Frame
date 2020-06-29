88f0ddf361c5cee96071ff76d567e979e817d8bc
MosesChang <1234512345>
2020-06-29 21:25:04 +0800

    first version from first example

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import "phaser";
+import { WelcomeScene } from "./welcomeScene";
+import { GameScene } from "./gameScene";
+import { ScoreScene } from "./scoreScene";
+
+const config: Phaser.Types.Core.GameConfig = {
+  title: "Starfall",
+  width: 800,
+  height: 600,
+  parent: "game",
+  scene: [WelcomeScene, GameScene, ScoreScene],
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
+  backgroundColor: "#18216D"
+};
+
+export class StarfallGame extends Phaser.Game {
+  constructor(config: Phaser.Types.Core.GameConfig) {
+    super(config);
+  }
+};
+
+window.onload = () => {
+  var game = new StarfallGame(config);
+};
